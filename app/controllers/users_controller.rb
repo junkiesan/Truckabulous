bf0f88eaec782f902b2f53d0395cffadd81b0de9
Julien Bouland <bouland.julien@gmail.com>
2020-05-18 17:15:06 +0200

    User controller and routes added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params_user)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(params_user)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def params_user
+    params.require(:user).permit(:email)
+  end
+end
