fddfb26578f55abcc27543e3ab9997256a9ea094
Julien Bouland <bouland.julien@gmail.com>
2020-05-18 20:31:11 +0200

    booking's model + controller are created

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,29 @@
+class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def booking_params
+    params.require(:booking).permit(:date, :number_of_participants)
+  end
+end
