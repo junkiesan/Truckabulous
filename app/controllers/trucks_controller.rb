237bd43a738f41d500bc7fcfb05c119381e9ba33
Kenza Mahresi <k.mahresi@gmail.com>
2020-05-19 13:06:37 +0200

    saving

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,4 +1,6 @@
 class TrucksController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @trucks = Truck.all
   end
@@ -13,17 +15,24 @@ class TrucksController < ApplicationController
 
   def create
     @truck = Truck.new(truck_params)
-    if @truck.save
+    @truck.user = current_user
+    if @truck.save!
       redirect_to truck_path(@truck)
     else
       render :new
     end
   end
 
+  def destroy
+    @truck = Truck.find(params[:id])
+    @truck.destroy
+    redirect_to truck_path
+  end
+
 private
 
   def truck_params
-    params.require(:truck).permit(:name)
+    params.require(:truck).permit(:name, :category, :price, :description)
   end
 
 end
