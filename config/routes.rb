552e17dd0a8f66961c421ed40766e10959368faf
Melanie Andral <m.andral@montpellier-bs.com>
2020-05-20 13:07:13 +0200

    View profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: 'trucks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :trucks, only: [:index, :show, :create, :new]
+
+  get "/profile", to: 'pages#profile'
+
 end
