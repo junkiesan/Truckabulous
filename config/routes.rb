e7db82ca988171678329aa5598ffc119dec2d48a
Kenza Mahresi <k.mahresi@gmail.com>
2020-05-20 13:12:32 +0200

    save

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'trucks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :trucks, only: [:index, :show, :create, :new]
+  resources :trucks, only: [:index, :show, :create, :new] do
+  resources :bookings, only: [:new, :create, :show, :delete]
+end
 end
