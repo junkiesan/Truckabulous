0b1fc4b72359fdcc3ed22fe35be4696d7378f91d
Melanie Andral <m.andral@montpellier-bs.com>
2020-05-22 11:54:14 +0200

    cards index and cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
+require 'open-uri'
 
 puts "Destroy Bookings"
 Booking.destroy_all
@@ -33,18 +34,50 @@ end
 kenza = User.create(name: 'Kenza', email: 'k.mahresi@gmail.com', password: 'kenza1')
 
 
-Truck.create!(name: 'Tacos Tuesday', category: 'Food', price: 150, description: 'The best Mexican tacos in town! Vegeterian options also available', user: kenza)
-Truck.create!(name: 'Sushi & Sashimi', category: 'Food', price: 200, description: 'Fresh fish for your sushi party!', user:User.first)
+truck_1 = Truck.new(name: 'Tacos Tuesday', category: 'Food', price: 150, description: 'The best Mexican tacos in town! Vegeterian options also available', user: kenza)
+file = URI.open('https://www.lemoniteurmateriels.fr/mediatheque/4/3/0/004509034_926x615_fb_ffffff.jpg')
+truck_1.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+truck_1.save!
 
-Truck.create!(name: 'Yoga Flow', category: 'Yoga', price: 190, description: 'Enjoy this unique Yoga class for a relaxed & zen attitude', user:User.first)
-Truck.create!(name: 'Dynamic Yoga', category: 'Yoga', price: 150, description: 'A mix of traditional Yoga with a dynamic twist!', user:User.first)
+truck_2 = Truck.new(name: 'Sushi & Sashimi', category: 'Food', price: 200, description: 'Fresh fish for your sushi party!', user:User.first)
+file = URI.open('https://www.lemoniteurmateriels.fr/mediatheque/4/3/0/004509034_926x615_fb_ffffff.jpg')
+truck_2.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+truck_2.save!
 
-Truck.create!(name: 'Spicy Cocktails', category: 'Cocktails', price: 120, description: 'Spice up your event with our amazing & delicious cocktails!', user: kenza)
-Truck.create!(name: 'Drink n Code', category: 'Cocktails', price: 100, description: 'Throwing a code AND drink party? This truck is for you!', user:User.first)
+# truck_3 = Truck.new(name: 'Yoga Flow', category: 'Yoga', price: 190, description: 'Enjoy this unique Yoga class for a relaxed & zen attitude', user:User.first)
+# file = URI.open('https://i.picsum.photos/id/10/2500/1667.jpg')
+# truck_3.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+# truck_3.save!
 
-Truck.create!(name: 'Puppy love', category: 'Pups & Kitties', price: 110, description: 'The truck for animal lovers! Half of total price goes to an animal shelter association!', user:User.first)
-Truck.create!(name: 'Mystic & Mysteries', category: 'Fortune Teller', price: 135, description: 'Want to know what the future holds for you? From Astrology reading to Tarot cards, this will be a mystical experience!', user:User.first)
-Truck.create!(name: 'Magic', category: 'Kids', price: 90, description: 'Magician, rabbits and all kinds of tricks for the best Kids party ever!', user:User.first)
+# truck_4 = Truck.new(name: 'Dynamic Yoga', category: 'Yoga', price: 150, description: 'A mix of traditional Yoga with a dynamic twist!', user:User.first)
+# file = URI.open('https://i.picsum.photos/id/10/2500/1667.jpg')
+# truck_4.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+# truck_4.save!
+
+# truck_5 = Truck.new(name: 'Spicy Cocktails', category: 'Cocktails', price: 120, description: 'Spice up your event with our amazing & delicious cocktails!', user: kenza)
+# file = URI.open('https://i.picsum.photos/id/10/2500/1667.jpg')
+# truck_5.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+# truck_5.save!
+
+# truck_6 = Truck.new(name: 'Drink n Code', category: 'Cocktails', price: 100, description: 'Throwing a code AND drink party? This truck is for you!', user:User.first)
+# file = URI.open('https://i.picsum.photos/id/10/2500/1667.jpg')
+# truck_6.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+# truck_6.save!
+
+# truck_7 = Truck.new(name: 'Puppy love', category: 'Pups & Kitties', price: 110, description: 'The truck for animal lovers! Half of total price goes to an animal shelter association!', user:User.first)
+# file = URI.open('https://i.picsum.photos/id/10/2500/1667.jpg')
+# truck_7.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+# truck_7.save!
+
+# truck_8 = Truck.new(name: 'Mystic & Mysteries', category: 'Fortune Teller', price: 135, description: 'Want to know what the future holds for you? From Astrology reading to Tarot cards, this will be a mystical experience!', user:User.first)
+# file = URI.open('https://i.picsum.photos/id/10/2500/1667.jpg')
+# truck_8.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+# truck_8.save!
+
+# truck_9 = Truck.new(name: 'Magic', category: 'Kids', price: 90, description: 'Magician, rabbits and all kinds of tricks for the best Kids party ever!', user:User.first)
+# file = URI.open('https://i.picsum.photos/id/10/2500/1667.jpg')
+# truck_9.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+# truck_9.save!
 
 
 # puts 'Creating truck...'
